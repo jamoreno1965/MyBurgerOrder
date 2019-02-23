77e785150b26af6314e1e32147784c534882e68a
Jesus Moreno <jamoreno1965@gmail.com>
2019-02-22 18:00:20 -0800

    add layout, burger, burgerIngredient, and css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/burgerBuilder/BurgerBuilder';
+
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<h1>Order Your Burger</h1>
+				<Layout>
+					<BurgerBuilder />
+				</Layout>
 			</div>
 		);
 	}
