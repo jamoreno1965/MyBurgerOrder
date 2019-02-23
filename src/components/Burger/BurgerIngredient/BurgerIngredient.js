f751564a69796ec322926684c7f6b4065fe41603
Jesus Moreno <jamoreno1965@gmail.com>
2019-02-23 04:40:08 -0800

    added burgerIngredient to layout

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import classes from './BurgerIngredient.module.css';
@@ -47,5 +47,9 @@ class BurgerIngredient extends Component {
 		return ingredient;
 	}
 }
+
+BurgerIngredient.propTypes = {
+	type : PropTypes.string.isRequired
+};
 
 export default BurgerIngredient;
